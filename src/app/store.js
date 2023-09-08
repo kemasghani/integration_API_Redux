6efe08c985200a6b0f9696fdffc074d778e93639
kemasghani <kemasghani123@gmail.com>
2023-09-08 23:57:48 +0700

    configure store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from "../features/authSlice";
+import dataReducer from "../features/dataSlice";
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer
-  },
+    reducer: {
+        auth: authReducer,
+        dataUser: dataReducer
+    },
 });
